ba1fee5d8ea739d05566b9cee4e48131a48faf0e
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-11-27 10:01:04 +0000

    Clean repo and apply .gitignore

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(["POST"])
+def register(request):
+    return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
